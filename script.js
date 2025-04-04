84c5e1fccfdd70c0873668676c04494f1ed07ad3
Joao-8598 <160759065+Joao-8598@users.noreply.github.com>
2025-04-04 08:03:13 -0300

    UpLoad

--- script.js
+++ script.js
@@ -25,19 +25,3 @@ function toggleDarkMode() {
     body.classList.toggle('dark-mode');
 }
 
-var estado = document.getElementById('estado');
-$('#onoff1').on('change', function toggleDarkMode() {
-    var el = this;
-    estado.innerHTML = el.checked ? 'ligado' : 'desligado';
-
-    // aqui podes juntar a lógica do ajax
-    $.ajax({
-        url: "some.php",
-        data: {
-            estado: this.checked
-        }
-    }).done(function(msg) {
-        if (msg == 'failed') return el.checked = !el.checked; // caso o servidor retorne "failed" mudar o estado do botão
-        else alert("Info gravada: " + msg);
-    });
-});
